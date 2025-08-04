2a936bf7a4771488794695902710e7a5aaf163c9
Moony2099 <160716037+Moonlite3751@users.noreply.github.com>
2025-08-04 13:22:34 +0200

    Initial commit

---
+++ TestThunderbolts/Test1.cs
@@ -0,0 +1,44 @@
+﻿using prjICE_Task2;
+namespace TestThunderbolts
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void testAge()
+        {
+            Thunderbolts t = new Thunderbolts(25, "John Walker", "Super soldier", "US Agent", "Asshole");
+            Assert.AreEqual(t[0], 25);
+        }
+
+        [TestMethod]
+        public void testName()
+        {
+            Thunderbolts t = new Thunderbolts(30, "Bob Reynolds", "A million explosive suns", "The Sentry", "Shy");
+            Assert.AreEqual(t["name"], "Bob Reynolds");
+        }
+
+        [TestMethod]
+        public void testPower()
+        {
+            Thunderbolts t = new Thunderbolts(32, "Yelena Belova", "Elite combat, espionage", "White Widow", "Blunt");
+            Assert.AreEqual(t[2], "Elite combat, espionage");
+        }
+
+        [TestMethod]
+        public void testChangedHeroName()
+        {
+            Thunderbolts t = new Thunderbolts(30, "Bob Reynolds", "A million explosive suns", "The Sentry", "Shy");
+            t[3] = "The Void";
+            Assert.AreEqual(t["heroName"], "The Void");
+        }
+
+        [TestMethod]
+        public void testHeroName()
+        {
+            Thunderbolts t = new Thunderbolts(30, "Alexei Shostakov", "Super strength, combat skills", "Alexei", "Loud");
+            t[3] = "Red Guardian";
+            Assert.AreEqual(t["heroName"], "Red Guardian");
+        }
+    }
+}
---
+++ prjICE-Task2/Thunderbolts.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjICE_Task2
+{
+    public class Thunderbolts
+    {
+        int age;
+        string name, power, heroName, personality;
+
+        public Thunderbolts(int age, string name, string power, string heroName, string personality)
+        {
+            this.age = age;
+            this.name = name;
+            this.power = power;
+            this.heroName = heroName;
+            this.personality = personality;
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                if(index == 0)
+                    return age;
+
+                else if(index == 1)
+                    return name;
+
+                else if(index == 2)
+                    return power;
+
+                else if(index == 3)
+                    return heroName;
+
+                else if(index == 4)
+                    return personality;
+
+                else
+                    return null;
+            }
+
+            set
+            {
+                if(index == 0)
+                    age = (int)value;
+
+                else if(index == 1)
+                    name = (string)value;
+
+                else if(index == 2)
+                    power = (string)value;
+
+                else if(index == 3)
+                    heroName = (string)value;
+
+                else if(index == 4)
+                    personality = (string)value;
+               
+            }
+        }
+
+        public object this[string attName]
+        {
+            get 
+            {
+                if(attName == "age")
+                    return age;
+
+                else if(attName == "name")
+                    return name;
+
+                else if(attName == "power")
+                    return power;
+
+                else if(attName == "heroName")
+                    return heroName;
+
+
+
+                else
+                    return null;
+            }
+
+            set
+            {
+                if (attName.ToLower().Equals("age"))
+                this.age = (int)value;
+                
+                else if (attName.ToLower().Equals("name"))
+                    this.name = (string)value;
+
+                else if (attName.ToLower().Equals("power"))
+                    this.power = (string)value;
+
+                else if (attName.ToLower().Equals("heroName"))
+                    this.heroName = (string)value;
+
+                else if (attName.ToLower().Equals("personality"))
+                    this.personality = (string)value;
+
+            }
+        }
+    }
+}
